c2c45ed64e3f7c6f354d912d9bd9f6b567481583
Olayemi Obagboye <richardoo@live.com>
2025-01-23 19:17:45 +0100

    Update vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/rest-countries-app/',
   plugins: [react()],
   css: {
     postcss: './postcss.config.js'
